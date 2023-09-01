da1e02f8ff7f5d25fb6987695b1277cdf4f3aa87
davide-rullo <davide.rullo01@gmail.com>
2023-09-01 15:33:11 +0200

    add added script for green squares

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,20 @@
 const gameTable = document.getElementById("game-table");
 const maxSquareNumber = 100;
 const playBtn = document.getElementById("start-btn")
-playBtn.addEventListener("click", function(){
+playBtn.addEventListener("click", function () {
     createTable(gameTable, 100)
 });
 
 
-function createTable (DOMelement, maxSquareNumber){
+function createTable(DOMelement, maxSquareNumber) {
     for (let i = 0; i < maxSquareNumber; i++) {
         const square = `<div class="square">${i + 1}</div>`;
         console.log(square);
         DOMelement.innerHTML += square;
         playBtn.classList.add("d-none");
+
+        square.addEventListener("click", function () {
+            square.classList.add("green-square");
+        })
     }
 }
