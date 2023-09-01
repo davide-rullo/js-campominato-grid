378c3ca8ecb532d9952d8ff6c19f42964e5eac63
davide-rullo <davide.rullo01@gmail.com>
2023-09-01 15:02:40 +0200

    add
    added first for loop for squares

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 
+const gameTable = document.getElementById("game-table");
+const maxSquareNumber = 100;
+
+for (let i = 0; i < maxSquareNumber; i++) {
+    const square = `<div class="square">${i + 1}</div>`;
+    console.log(square);
+    gameTable.innerHTML += square;
+}
