3d86576d172a4c326c886c4c22d88b31941b0574
davide-rullo <davide.rullo01@gmail.com>
2023-09-01 15:07:34 +0200

    add
    added function to create game table

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,12 @@
 const gameTable = document.getElementById("game-table");
 const maxSquareNumber = 100;
 
-for (let i = 0; i < maxSquareNumber; i++) {
-    const square = `<div class="square">${i + 1}</div>`;
-    console.log(square);
-    gameTable.innerHTML += square;
+
+
+function createTable (DOMelement, maxSquareNumber){
+    for (let i = 0; i < maxSquareNumber; i++) {
+        const square = `<div class="square">${i + 1}</div>`;
+        console.log(square);
+        DOMelement.innerHTML += square;
+    }
 }
